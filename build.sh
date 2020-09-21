cba0ebdc94d1af105d5b5a1d95526619f0fb2046
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-09-21 21:10:02 +0200

    correct folder names

--- build.sh
+++ build.sh
@@ -11,11 +11,10 @@ apt -qyy install \
 	wget \
 	pkg-config \
 	libudev-dev
-
 VERSION=1.7.l.4.11.0-00001
 wget https://01.org/sites/default/files/downloads/qat$VERSION.tar.gz -O qat-$VERSION.tar.gz
 tar xvf qat-$VERSION.tar.gz -C qat-$VERSION
-chown -R root:root  QAT
+chown -R root:root  qat-$VERSION
 cd qat-$VERSION
 ./configure --enable-kapi
 make
