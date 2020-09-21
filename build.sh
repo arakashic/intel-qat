bf9835d19b5ab3fce20de843c398e318b9db0093
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-09-21 23:37:39 +0200

    folder shuffle

--- build.sh
+++ build.sh
@@ -15,10 +15,10 @@ apt -qyy install \
 
 VERSION=1.7.l.4.10.0-00014
 wget https://01.org/sites/default/files/downloads/qat$VERSION.tar.gz -O qat$VERSION.tar.gz
-mkdir qat-$VERSION
-tar xvf qat$VERSION.tar.gz -C qat-$VERSION
-chown -R root:root  qat-$VERSION
-cd qat-$VERSION
+mkdir intel-qat
+tar xvf qat$VERSION.tar.gz -C intel-qat
+chown -R root:root  intel-qat
+cd intel-qat
 patch -p1 < ../patch/0001-pci_aer.patch
 patch -p1 < ../patch/0001-timespec.patch
 patch -p1 < ../patch/0001-cryptohash.patch
