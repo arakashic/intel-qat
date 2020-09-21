9739cb6356031a7b1688efe5735277cd52b07d6b
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-09-21 21:02:47 +0200

    Download build dependencies

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/sh -ex
+
+# Download kernel and headers
+KERNEL_VERSION=$(uname -r)
+#export KERNEL_SOURCE_ROOT=/lib/modules/${KERNEL_VERSION}/build
+
+apt -qyy install \
+	linux-image-${KERNEL_VERSION}  \
+	linux-headers-${KERNEL_VERSION} \
+	pciutils \
+	wget \
+	pkg-config \
+	libudev-dev
+
 VERSION=1.7.l.4.11.0-00001
 wget https://01.org/sites/default/files/downloads/qat$VERSION.tar.gz -O qat-$VERSION.tar.gz
 tar xvf qat-$VERSION.tar.gz -C qat-$VERSION
