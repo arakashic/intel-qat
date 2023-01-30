42980e79328734ba216b3e295adca1731ff4e402
Yanfei Guo <yguo@anl.gov>
2023-01-30 09:05:01 -0600

    update build script

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@
 # ifndef KSRC
 # export KERNEL_SOURCE_ROOT=/lib/modules/${KVERS}/build
 # else
+export KVERS=$(shell apt info linux-headers-truenas-amd64 | awk '/Source:/ { print $$2}' | sed 's/linux-//')
+export KSRC="/usr/src/linux-headers-${KVERS}"
 export KERNEL_SOURCE_ROOT=${KSRC}
 # endif
 
