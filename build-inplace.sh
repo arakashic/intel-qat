70db9bd03361f16e8a4f73b47b9d8a192cd6ac71
Yanfei Guo <yguo@anl.gov>
2023-02-02 21:19:33 -0600

    fix

--- build-inplace.sh
+++ build-inplace.sh
@@ -30,7 +30,7 @@ apt -qyy install \
 # VERSION=1.7.l.4.10.0-00014
 # wget https://01.org/sites/default/files/downloads/qat$VERSION.tar.gz -O qat$VERSION.tar.gz
 mkdir -p /intel-qat
-tar xvf QAT.L.4.20.0-00001.tar.gz -c /intel-qat
+tar xvf QAT.L.4.20.0-00001.tar.gz -C /intel-qat
 chown -R root:root /intel-qat
 cd /intel-qat
 ./configure --enable-kapi
