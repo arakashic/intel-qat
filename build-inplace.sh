024799ab0d3300ab3e2f6aa344c44d3d868a8859
Yanfei Guo <yguo@anl.gov>
2023-02-02 21:08:06 -0600

    Add inplace build

---
+++ build-inplace.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -ex
+
+# Download kernel and headers
+# ifndef KVERS
+# KVERS=$(shell uname -r)
+# endif
+
+# ifndef KSRC
+# export KERNEL_SOURCE_ROOT=/lib/modules/${KVERS}/build
+# else
+# apt info linux-headers-truenas-amd64
+# export KVERS=$(apt info linux-headers-truenas-amd64 | awk '/Source:/ { print $2}' | sed 's/linux-//')
+# export KSRC="/usr/src/linux-headers-${KVERS}"
+# export KERNEL_SOURCE_ROOT=${KSRC}
+# export KERNEL_SOURCE_ROOT=/lib/modules/5.10.0-18-amd64/build
+export KERNEL_SOURCE_ROOT=/lib/modules/5.15.79+truenas/build
+ls /usr/src/
+ls /lib/modules/
+# endif
+
+
+apt -qyy install \
+	pciutils \
+	yasm \
+	wget \
+	pkg-config \
+	libboost-all-dev \
+	libudev-dev
+
+# VERSION=1.7.l.4.10.0-00014
+# wget https://01.org/sites/default/files/downloads/qat$VERSION.tar.gz -O qat$VERSION.tar.gz
+tar xvf QAT.L.4.20.0-00001.tar.gz
+./configure --enable-kapi
+make
+make install
