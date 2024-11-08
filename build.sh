2adb720fcb4dd468017fd0c717baddbf0605bf1e
Yanfei Guo <yguo@anl.gov>
2024-11-08 01:22:48 -0600

    Update QAT to latest version for HW1.1

--- build.sh
+++ build.sh
@@ -13,9 +13,9 @@
 # export KSRC="/usr/src/linux-headers-${KVERS}"
 # export KERNEL_SOURCE_ROOT=${KSRC}
 # export KERNEL_SOURCE_ROOT=/lib/modules/5.10.0-18-amd64/build
-export KERNEL_SOURCE_ROOT=/lib/modules/5.15.79+truenas/build
-ls /usr/src/
-ls /lib/modules/
+# export KERNEL_SOURCE_ROOT=/lib/modules/5.15.79+truenas/build
+# ls /usr/src/
+# ls /lib/modules/
 # endif
 
 
@@ -29,7 +29,7 @@ apt -qyy install \
 # VERSION=1.7.l.4.10.0-00014
 # wget https://01.org/sites/default/files/downloads/qat$VERSION.tar.gz -O qat$VERSION.tar.gz
 mkdir intel-qat
-tar xvf QAT.L.4.20.0-00001.tar.gz -C intel-qat
+tar xvf ./QAT.L.4.26.0-00008.tar.gz -C intel-qat
 chown -R root:root  intel-qat
 cd intel-qat
 ./configure --enable-kapi --enable-icp-sriov=host
