0c08a97ad96eee6a6b5b4103b9c13f4abaed9497
Yanfei Guo <yguo@anl.gov>
2024-12-10 13:34:08 -0600

    update driver

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #!/bin/sh -ex
 
-export KVERS=$(apt info linux-headers-truenas-production-amd64 | awk '/Source:/ { print $2 }' | sed 's/linux-//')
-export KSRC="/lib/modules/${KVERS}/build"
-export KERNEL_SOURCE_ROOT=${KSRC}
-# env
-
-# apt -qyy install wget
-
-# VERSION=1.7.l.4.10.0-00014
-# wget https://01.org/sites/default/files/downloads/qat$VERSION.tar.gz -O qat$VERSION.tar.gz
-tar xvf ./QAT.L.4.26.0-00008.tar.gz -C .
+tar xvf ./QAT.L.4.27.0-00006.tar.gz -C .
